ed2115179be4174e4ccd00ff87aa78348eafee8e
Blackbrv <luthfiprtm@gmail.com>
2026-02-23 02:18:24 +0700

    Update: Tanstack initialization

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,16 +5,39 @@ import Root from "./src/root";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { ThemeProvider } from "./components/ui/theme-provider";
 import { Toaster } from "./components/ui/sonner";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const element = document.getElementById("root");
 const root = ReactDOM.createRoot(element as HTMLElement);
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            // the query will not refetch on window focus.
+            refetchOnWindowFocus: false,
+            // failed queries will not retry by default.
+            retry: false,
+            // query will always fetch and ignore the online / offline state.
+            networkMode: "always",
+        },
+        mutations: {
+            // failed mutations will not retry.
+            retry: false,
+            // query will always fetch and ignore the online / offline state.
+            networkMode: "always",
+        },
+    },
+});
+
 root.render(
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <TooltipProvider>
-            <Root />
-            <Toaster />
-        </TooltipProvider>
-    </ThemeProvider>,
+    <QueryClientProvider client={queryClient}>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <TooltipProvider>
+                <Root />
+                <Toaster />
+            </TooltipProvider>
+        </ThemeProvider>
+    </QueryClientProvider>,
 );
 
 export {};
